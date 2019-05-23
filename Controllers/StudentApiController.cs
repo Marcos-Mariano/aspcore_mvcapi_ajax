6cf8dc0f9d7dce2c5288b68b4d45c5fd91575537
Marcos-Mariano <marcosvms@gmail.com>
2019-05-23 09:32:08 -0300

    Project Setup and get method

---
+++ Controllers/StudentApiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apitestes.Models.Student;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apitestes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentApiController : ControllerBase
+    {
+        
+            // GET: api/GetAllStudents  
+            [HttpGet]
+            public IEnumerable<PersonalDetail> GetAllStudents()
+            {
+                List<PersonalDetail> students = new List<PersonalDetail>
+           {
+           new PersonalDetail{
+                              RegNo = "2017-0001",
+                              Name = "Nishan",
+                              Address = "Kathmandu",
+                              PhoneNo = "9849845061",
+                              AdmissionDate = DateTime.Now
+                              },
+           new PersonalDetail{
+                              RegNo = "2017-0002",
+                              Name = "Namrata Rai",
+                              Address = "Bhaktapur",
+                              PhoneNo = "9849845062",
+                              AdmissionDate = DateTime.Now
+                             },
+           };
+                return students;
+            }
+        }
+
+
+
+
+
+    }
